afbee441a15d3e6d16f442c760484ef5f9ce6a8f
khanhdd <khanhdd@neo.vn>
2024-05-04 23:50:00 +0700

    delete flit and leap plugins

--- lua/kickstart/plugins/extra-plugins.lua
+++ lua/kickstart/plugins/extra-plugins.lua
@@ -126,20 +126,6 @@ return {
     'editorconfig/editorconfig-vim',
   },
 
-  -- Enhanced f/t motions for Leap
-  {
-    'ggandor/flit.nvim',
-    keys = function()
-      ---@type LazyKeys[]
-      local ret = {}
-      for _, key in ipairs { 'f', 'F', 't', 'T' } do
-        ret[#ret + 1] = { key, mode = { 'n', 'x', 'o' }, desc = key }
-      end
-      return ret
-    end,
-    opts = { labeled_modes = 'nx' },
-  },
-
   -- breadcrumbs
   -- {
   --   "LunarVim/breadcrumbs.nvim",
